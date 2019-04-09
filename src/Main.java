abe5ee23c9ae7e4fa6c07788009a7f7c4ac5beef
Matthew Yarmolinsky <myarmolinsky3043@bths.edu>
2019-04-09 11:57:24 -0400

    Worked on viewOwnedPokemon()

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		ArrayList<Pokemon> pokedex = new ArrayList<>();
 		fillPossibilities(pokedex);
 		Player p = new Player();
-		int[] spawnTracker = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		int[] spawnRateCounter = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 		boolean finished = false;
 		while(!finished) {
 			System.out.println("Input the number corresponding to your choice:");
@@ -15,43 +15,92 @@ public class Main {
 			System.out.println("2) View Owned Pokemon");
 			System.out.println("3) Breed Pokemon");
 			System.out.println("4) Quit");
+			System.out.println();
 			switch(input.nextLine().charAt(0)){
-				case '1':
-					search(pokedex, p, null);
-					break;
-				case '2':
-					viewOwnedPokemon();
-					break;
-				case '3':
-					breed();
-					break;
-				case '4':
-					finished = true;
-					break;
-				default:
-					System.out.println("Input does not match an available choice.");
+			case '1':
+				System.out.println();
+				search(pokedex, p, spawnRateCounter);
+				break;
+			case '2':
+				System.out.println();
+				viewOwnedPokemon(p);
+				break;
+			case '3':
+				System.out.println();
+				breed();
+				break;
+			case '4':
+				finished = true;
+				System.out.println();
+				System.out.println("Quitting ...");
+				break;
+			default:
+				System.out.println();
+				System.out.println("Input does not match an available choice.");
+				System.out.println();
 			}
 		}
 		input.close();
+	}	
+
+	private static void fillPossibilities(ArrayList<Pokemon> pokedex) {
+		pokedex.add(new Pokemon("Bulbasaur", "Grass", "Poison", 45, 49, 49, 65, 65, 45, 1, new String[] {"monster", "grass"}, 0.875));
 	}
 
-	private static void search(ArrayList<Pokemon> pokedex, Player p, int[] spawnTracker) {
+	private static void search(ArrayList<Pokemon> pokedex, Player p, int[] spawnRateCounter) {
 		int rand = new Random().nextInt(5);
-		if (rand == 0)
+		if (rand == 0) {
 			System.out.println("You found nothing. :(");
+			System.out.println();
+		}
 		else {
-			rand = new Random().nextInt(pokedex.size()-1);
-			if(spawnTracker[pokedex.get(rand).getSpawnRate()] != pokedex.get(rand).getSpawnRate()) {
+			rand = new Random().nextInt(pokedex.size());
+			if(spawnRateCounter[pokedex.get(rand).getSpawnRate()] != pokedex.get(rand).getSpawnRate()) {
 				// check if spawn rate of the pokemon has been reached
 				// (the rare the pokemon, the higher the the number required to be reached)
 				// if the spawn rate has not been reached, increment by 1 and re-search
-				spawnTracker[pokedex.get(rand).getSpawnRate()]++;
-				search(pokedex, p, spawnTracker);
+				spawnRateCounter[pokedex.get(rand).getSpawnRate()]++;
+				search(pokedex, p, spawnRateCounter);
 			} else {
 				// if spawn rate HAS been reached, give that pokemon to the player
-				spawnTracker[pokedex.get(rand).getSpawnRate()] = 0;
+				spawnRateCounter[pokedex.get(rand).getSpawnRate()] = 0;
 				p.catchPokemon(new OwnedPokemon(pokedex.get(rand)));
+				System.out.println("You caught a " + pokedex.get(rand).getName() + "!");
+				System.out.println();
 			}
+		}
+	}
+
+	private static void viewOwnedPokemon(Player p, Scanner sc) {
+		System.out.println("Your Pokemon:");
+		for (int i = 0; i < p.getPC().size(); i++) {
+			System.out.println(i + 1 + ") " + p.getPC().get(i).getNickname());
+		}
+		System.out.println();
+		System.out.println("Would you like to view any pokemon in particular?");
+		System.out.println("1) Enter the number correlating to a pokemon in your PC.");
+		System.out.println("2) Enter \"0\" to go back to the previous menu.");
+		System.out.println();
+		String temp = sc.next();
+		while(!isNumeric(temp)) {
+			System.out.println("Input does not match an avalable choice");
+			System.out.println();
+			System.out.println("Would you like to view any pokemon in particular?");
+			System.out.println("1) Enter the number correlating to a pokemon in your PC.");
+			System.out.println("2) Enter \"0\" to go back to the previous menu.");
+			System.out.println();
+		}
+		while(Integer.parseInt(temp) != 0) {
+			
+		}
+	}
+
+	private static boolean isNumeric(String temp) {
+		try {
+			Integer.parseInt(temp);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
 		}
 	}
 
@@ -59,12 +108,4 @@ public class Main {
 		// to be implemented
 	}
 
-	private static void viewOwnedPokemon() {
-		// to be implemented
-	}
-
-	private static void fillPossibilities(ArrayList<Pokemon> pokedex) {
-		pokedex.add(new Pokemon("Bulbasaur", "Grass", "Poison", 45, 49, 49, 65, 65, 45, 27, new String[] {"monster", "grass"}, 0.875));
-	}
-	
 }
--- src/Player.java
+++ src/Player.java
@@ -2,16 +2,18 @@ import java.util.*;
 
 public class Player {
 	
-	private ArrayList<Pokemon> pokedex;
 	private ArrayList<OwnedPokemon> pc;
 	
 	public Player() {
-		pokedex = new ArrayList<>();
 		pc = new ArrayList<>();
 	}
 
 	public void catchPokemon(OwnedPokemon ownedPokemon) {
 		pc.add(ownedPokemon);
+	}
+
+	public ArrayList<OwnedPokemon> getPC() {
+		return pc;
 	}
 
 }
