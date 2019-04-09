453afbc4647ea2042c3c450478695af957e23c2f
Matthew Yarmolinsky <myarmolinsky3043@bths.edu>
2019-04-08 21:44:27 -0400

    Worked on Main Class/Menu

--- src/main.java
+++ src/main.java
@@ -5,6 +5,44 @@ public class Main {
 	public static void main(String[] args) {
 		ArrayList<Pokemon> pokedex = new ArrayList<>();
 		fillPossibilities(pokedex);
+		Scanner input = new Scanner(System.in);
+		boolean finished = false;
+		while(!finished) {
+			System.out.println("Input the number corresponding to your choice:");
+			System.out.println("1) Search for Pokemon");
+			System.out.println("2) View Owned Pokemon");
+			System.out.println("3) Breed Pokemon");
+			System.out.println("4) Quit");
+			switch(input.nextLine().charAt(0)){
+				case '1':
+					search();
+					break;
+				case '2':
+					viewOwned();
+					break;
+				case '3':
+					breed();
+					break;
+				case '4':
+					finished = true;
+					break;
+				default:
+					System.out.println("Input does not match an available choice.");
+			}
+		}
+		input.close();
+	}
+
+	private static void breed() {
+		// to be implemented
+	}
+
+	private static void viewOwned() {
+		// to be implemented
+	}
+
+	private static void search() {
+		// to be implemented
 	}
 
 	private static void fillPossibilities(ArrayList<Pokemon> pokedex) {
