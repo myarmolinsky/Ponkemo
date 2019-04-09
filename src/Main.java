a1ab69e84f357ac61d0401d46db3adb25d607a87
Matthew Yarmolinsky <myarmolinsky3043@bths.edu>
2019-04-09 11:34:09 -0400

    Made search() method
    Made search() method, but need to make viewOwnedPokemon() method in order to test functionality

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		ArrayList<Pokemon> pokedex = new ArrayList<>();
 		fillPossibilities(pokedex);
-		Scanner input = new Scanner(System.in);
+		Player p = new Player();
+		int[] spawnTracker = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 		boolean finished = false;
 		while(!finished) {
 			System.out.println("Input the number corresponding to your choice:");
@@ -15,10 +17,10 @@ public class Main {
 			System.out.println("4) Quit");
 			switch(input.nextLine().charAt(0)){
 				case '1':
-					search();
+					search(pokedex, p, null);
 					break;
 				case '2':
-					viewOwned();
+					viewOwnedPokemon();
 					break;
 				case '3':
 					breed();
@@ -33,15 +35,31 @@ public class Main {
 		input.close();
 	}
 
+	private static void search(ArrayList<Pokemon> pokedex, Player p, int[] spawnTracker) {
+		int rand = new Random().nextInt(5);
+		if (rand == 0)
+			System.out.println("You found nothing. :(");
+		else {
+			rand = new Random().nextInt(pokedex.size()-1);
+			if(spawnTracker[pokedex.get(rand).getSpawnRate()] != pokedex.get(rand).getSpawnRate()) {
+				// check if spawn rate of the pokemon has been reached
+				// (the rare the pokemon, the higher the the number required to be reached)
+				// if the spawn rate has not been reached, increment by 1 and re-search
+				spawnTracker[pokedex.get(rand).getSpawnRate()]++;
+				search(pokedex, p, spawnTracker);
+			} else {
+				// if spawn rate HAS been reached, give that pokemon to the player
+				spawnTracker[pokedex.get(rand).getSpawnRate()] = 0;
+				p.catchPokemon(new OwnedPokemon(pokedex.get(rand)));
+			}
+		}
+	}
+
 	private static void breed() {
 		// to be implemented
 	}
 
-	private static void viewOwned() {
-		// to be implemented
-	}
-
-	private static void search() {
+	private static void viewOwnedPokemon() {
 		// to be implemented
 	}
 
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,17 @@
+import java.util.*;
 
 public class Player {
+	
+	private ArrayList<Pokemon> pokedex;
+	private ArrayList<OwnedPokemon> pc;
+	
+	public Player() {
+		pokedex = new ArrayList<>();
+		pc = new ArrayList<>();
+	}
+
+	public void catchPokemon(OwnedPokemon ownedPokemon) {
+		pc.add(ownedPokemon);
+	}
 
 }
